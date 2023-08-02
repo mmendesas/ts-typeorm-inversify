ec29171ad38b462b35241157b61541587f33c94a
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2023-08-01 22:31:59 -0300

    feat: add repository pattern start

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,43 @@ import 'dotenv/config';
 
 import { App } from './app';
 
+import Database, { IUser } from './utils/Database';
+
 console.clear();
+
+export class UserRepository {
+  private readonly _db = Database;
+
+  async createUser(user: IUser) {
+    return this._db.create(user);
+  }
+}
+
+export class UserService {
+  constructor(private readonly _repo: UserRepository) {}
+
+  async createUser(user: IUser) {
+    return this._repo.createUser(user);
+  }
+}
+
+export class UserController {
+  constructor(private readonly _service: UserService) {}
+
+  async createUser(user: IUser) {
+    return this._service.createUser(user);
+  }
+}
 
 (async () => {
   try {
+    const repo = new UserRepository();
+    const service = new UserService(repo);
+    const controller = new UserController(service);
+
+    controller.createUser({ username: 'Marcio' }).then(console.log);
+    controller.createUser({ username: 'Mendes' }).then(console.log);
+
     new App().setup();
   } catch (err) {
     console.error('[server] Something went wrong', err);
